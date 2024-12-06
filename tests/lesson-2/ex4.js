3e31236300d200b155e8f56b45954effbf4e8fe7
Udemy-My <tramyltt@gmail.com>
2024-12-06 21:55:55 +0700

    lesson-2

---
+++ tests/lesson-2/ex4.js
@@ -0,0 +1,7 @@
+const myTall = 150;
+let canNangLyTuong = (myTall - 100)*9/10;
+let canNangToiDa = myTall - 100;
+let canNangToiThieu = canNangToiDa *8/10;
+console.log("Cân nặng lý tưởng của bạn là :" + canNangLyTuong);
+console.log("Cân nặng tối đa của bạn là :" + canNangToiDa);
+console.log("Cân nặng tối thiểu của bạn là :" + canNangToiThieu);
---
+++ tests/lesson-2/ex8.js
@@ -0,0 +1,3 @@
+for(let i = 4; i<=100; i=i+4){
+    console.log(i);
+};
